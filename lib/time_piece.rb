f60b7973e8957b9529f88f08adfca1b0088d7773
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-11 17:05:15 +0000

    Passed.4 - added class TimePiece and Time.new as attribute

--- lib/time_piece.rb
+++ lib/time_piece.rb
@@ -1,4 +1,13 @@
 class TimePiece
 
+  attr_reader :date
+
+  def initialize
+    @date = Time.new.yday
+  end
+
+  def today_date
+    @date
+  end
 
 end
--- spec/time_piece_spec.rb
+++ spec/time_piece_spec.rb
@@ -1,10 +1,10 @@
 require 'time_piece'
 require 'date'
 
-describe Time_piece do
+describe TimePiece do
   describe "#initialize" do
     it 'test that instantiates with todays date' do
-      expect(subject.todays_date).to eq Time.new
+      expect(subject.today_date).to eq Time.new.yday
     end
   end
 end
