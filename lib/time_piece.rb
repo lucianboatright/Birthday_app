689a9e45640fbf876d1a2ef079499aaf2b815efd
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-11 17:39:00 +0000

    Passed.5 - added initialized attr_reader

--- lib/time_piece.rb
+++ lib/time_piece.rb
@@ -1,9 +1,24 @@
 class TimePiece
 
-  attr_reader :date
+  attr_reader :date, :name, :day, :month
 
-  def initialize
+  def initialize(name,day,month)
     @date = Time.new.yday
+    @name = name
+    @day = day
+    @month = month
+  end
+
+  def date
+    @date
+  end
+
+  def month
+    @month
+  end
+
+  def name
+    @name
   end
 
 end
--- spec/time_piece_spec.rb
+++ spec/time_piece_spec.rb
@@ -3,10 +3,22 @@ require 'date'
 
 describe TimePiece do
   describe "#initialize" do
+    let(:subject) { described_class.new("james", 1, 1)}
     it 'test that instantiates with todays date' do
-      expect(subject.date).to eq Time.new.yday
+      expect(subject.date).to eq(11)
+    end
+    it 'test that instantiates with todays day' do
+      expect(subject.day).to eq(1)
+    end
+    it 'test that instantiates with todays month' do
+      expect(subject.month).to eq(1)
+    end
+
+    it 'test that instantiates with todays date' do
+      expect(subject.name).to eq('james')
     end
   end
+
 end
 
 
