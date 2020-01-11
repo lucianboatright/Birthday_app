42da35e6e5e05de8fec31067ca27c8f783cb4ed7
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-11 18:05:42 +0000

    added dependecy injection for name,date,month into Timepiec.new

--- app.rb
+++ app.rb
@@ -7,15 +7,33 @@ class Birthday < Sinatra::Base
   end
 
   post '/take_name' do
-    p params
     #can i put all three params in one
-    @users = params[:name]
-    @date = params[:date] 
-    @month = params[:month]
+    name = params[:name]
+    date = params[:date] 
+    month = params[:month]
+    @user = TimePiece.new(name, date, month)
     erb :form  #User.new(params[:name])
   end
 
-  # get '/show_bday' do 
 
   run! if app_file == $0
 end
+
+# if tday = bday
+
+
+  # get '/days_till' do
+  #   @users = params[:name]
+  #   @date = params[:date] 
+  #   @month = params[:month]
+  #   erb :days
+
+
+  # player_1 = Player.new(params[:player_1_name])
+  # player_2 = Player.new(params[:player_2_name])
+  # @game = Game.new(player_1, player_2)
+  # redirect '/play'
+
+  # get '/show_bday' do 
+
+
--- spec/feature/enter_name_spec.rb
+++ spec/feature/enter_name_spec.rb
@@ -2,7 +2,7 @@
 feature 'name form' do
   scenario 'check name equals user input from form' do
     visit '/'
-    fill_in :name, with: "David"
+    fill_in 'name', with: "David"
     click_button 'Complete'
 
     expect(page).to have_content "David Happy Birthday"
