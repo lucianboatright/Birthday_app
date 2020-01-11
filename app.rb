16f0c082156e8dff4f0f65210c64f2b41056701b
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-11 13:49:11 +0000

    Passed.1 test for text on home page

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class Birthday < Sinatra::Base
 
-  # enable
 
-  # get '/' do
-  #   erb :home
-  # end
+
+  get '/' do
+    erb :home
+  end
 
   run! if app_file == $0
 end
